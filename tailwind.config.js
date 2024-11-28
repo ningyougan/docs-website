90df9e24cd60b0952a872d29435bfa3d84273ab0
Polaris <ningyg@spdb.com.cn>
2024-11-28 14:59:49 +0800

    feat: add content markdown styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+import tailwindMdBase from '@geoffcodesthings/tailwind-md-base';
 export default {
   // purge: ['./src/components/Content.vue'],
   content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      markdownBase: {
+        img: {
+          maxWidth: '20%',
+          borderWidth: defaultTheme.borderWidth.default,
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [tailwindMdBase()],
 };
